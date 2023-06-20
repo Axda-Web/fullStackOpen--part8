b730fd9db286febef89bfef54afd00c9e54393e9
Alyx DARENNE <axda.web@gmail.com>
2023-06-20 15:19:13 +0200

    part8/ex_21: Add filter by genre (GraphQL) feature

--- library-frontend/src/pages/Books.js
+++ library-frontend/src/pages/Books.js
@@ -1,43 +1,42 @@
 import { useState } from 'react';
 import { useQuery } from '@apollo/client';
-import { ALL_BOOKS } from '../queries';
+import { ALL_BOOKS, GENRES } from '../queries';
 
 const Books = () => {
-	const [genre, setGenre] = useState('');
-	const [filteredBooks, setFilteredBooks] = useState([]);
-	const { loading, error, data } = useQuery(ALL_BOOKS);
+	const [selectedGenre, setSelectedGenre] = useState('');
 
-	if (loading) {
+	const {
+		loading: booksLoading,
+		error: booksError,
+		data: booksData
+	} = useQuery(ALL_BOOKS, {
+		variables: { genre: selectedGenre }
+	});
+
+	const { genresLoading, genresError, data: genresData } = useQuery(GENRES);
+
+	if (booksLoading || genresLoading) {
 		return <div>loading...</div>;
 	}
 
-	if (error) {
-		return <div>{error.message}</div>;
+	if (booksError || genresError) {
+		return <div>Error</div>;
 	}
-
-	const genres = [
-		...Array.from(new Set(data.allBooks.flatMap((a) => a.genres))),
-		'All genres'
-	];
 
 	const handleFilterClick = (e) => {
 		if (e.target.textContent === 'All genres') {
-			setGenre('');
-			setFilteredBooks(data.allBooks);
+			setSelectedGenre('');
 			return;
 		}
-		setGenre(e.target.textContent);
-		const filteredByGenre = data.allBooks.filter((b) =>
-			b.genres.includes(e.target.textContent)
-		);
-		setFilteredBooks(filteredByGenre);
+		setSelectedGenre(e.target.textContent);
 	};
 	return (
 		<div>
 			<h2>books</h2>
-			{genre && (
+			{selectedGenre && (
 				<p>
-					in genre <span style={{ fontWeight: 'bold' }}>{genre}</span>
+					in genre{' '}
+					<span style={{ fontWeight: 'bold' }}>{selectedGenre}</span>
 				</p>
 			)}
 			<table>
@@ -47,35 +46,29 @@ const Books = () => {
 						<th>author</th>
 						<th>published</th>
 					</tr>
-					{filteredBooks.length
-						? filteredBooks.map((a) => (
-								<tr key={a.title}>
-									<td>{a.title}</td>
-									<td>{a.author.name}</td>
-									<td>{a.published}</td>
-								</tr>
-						  ))
-						: data.allBooks.map((a) => (
-								<tr key={a.title}>
-									<td>{a.title}</td>
-									<td>{a.author.name}</td>
-									<td>{a.published}</td>
-								</tr>
-						  ))}
+					{booksData?.allBooks.map((a) => (
+						<tr key={a.title}>
+							<td>{a.title}</td>
+							<td>{a.author.name}</td>
+							<td>{a.published}</td>
+						</tr>
+					))}
 				</tbody>
 			</table>
 			<div style={{ marginTop: '10px' }}>
-				{genres.map((g, i) => (
-					<button
-						key={i}
-						style={{
-							marginRight: '10px'
-						}}
-						onClick={handleFilterClick}
-					>
-						{g}
-					</button>
-				))}
+				{genresData?.genres
+					.concat(['All genres'])
+					.map((genre, index) => (
+						<button
+							key={index}
+							style={{
+								marginRight: '10px'
+							}}
+							onClick={handleFilterClick}
+						>
+							{genre}
+						</button>
+					))}
 			</div>
 		</div>
 	);
